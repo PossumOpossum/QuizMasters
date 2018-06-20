0f2f3c94dc3c8f400d321b299403090374d54fdc
Иван Цветановски <ivan.cvetanovski@students.finki.ukim.mk>
2018-06-20 23:44:20 +0200

    Final commit...Yea right

--- QuizMasters-master/QuizMasters/LeaderBoard.Designer.cs
+++ QuizMasters-master/QuizMasters/LeaderBoard.Designer.cs
@@ -41,10 +41,10 @@
             this.surname,
             this.points});
             this.listView1.GridLines = true;
-            this.listView1.Location = new System.Drawing.Point(32, 29);
+            this.listView1.Location = new System.Drawing.Point(17, 17);
             this.listView1.Margin = new System.Windows.Forms.Padding(8);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(2068, 880);
+            this.listView1.Size = new System.Drawing.Size(1584, 913);
             this.listView1.TabIndex = 0;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
